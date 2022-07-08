d4b011110d9ddd563abbb628c43bf2e33a860b36
PotatoHY <oscarmaths9141@gmail.com>
2022-07-09 01:32:14 +0200

    first commit

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,102 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/vocab'}>
+				<a sveltekit:prefetch href="/vocab">Vocabulary by Chapter</a>
+			</li>
+			<li class:active={$page.url.pathname === '/verbs'}>
+				<a sveltekit:prefetch href="/verbs">Verb forms</a>
+			</li>
+			<li class:active={$page.url.pathname === '/grammar'}>
+				<a sveltekit:prefetch href="/grammar">Grammar Express</a>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="corner">
+		<a href="https://potatohy.github.com">GitHub</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		color: rgb(0, 90, 80);
+		text-decoration: none;
+		font-family: "Trebuchet MS";
+		font-weight: bold;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgb(199, 195, 195);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--accent-color);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: var(--heading-color);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--accent-color);
+	}
+</style>
---
+++ src/routes/vocab.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { vocabs } from '../lib/stores'
+  vocabs.useLocalStorage();
+
+  let article = '';
+  let word = '';
+  let plural = '';
+
+  function addVocab() {
+    $vocabs.push({article, word, plural})
+    $vocabs = $vocabs;
+  }
+</script>
+
+<main>
+  <table>
+    <tr>
+      <td>Article</td>
+      <td>Word</td>
+      <td>Plural</td>
+    </tr>
+    {#each $vocabs as vocab}
+      <tr>
+        <td>{vocab.article}</td>
+        <td>{vocab.word}</td>
+        <td>{vocab.plural}</td>
+      </tr>
+    {/each}
+
+  </table>
+
+  <select bind:value={article}>
+    <option value="der">der</option>
+    <option value="die">die</option>
+    <option value="das">das</option>
+  </select>
+  <input type="text" bind:value={word}>
+  <input type="text" bind:value={plural}>
+  <button on:click={addVocab}>Add Vocabulary</button>
+</main>
